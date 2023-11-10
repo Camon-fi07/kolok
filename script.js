db7e3e2ce40e3a02e2fffd1b8cbc354de354c3e8
Camon-fi07 <422nikitos244@mail.ru>
2023-11-10 20:59:53 +0700

    test request

--- script.js
+++ script.js
@@ -49,3 +49,7 @@ list.addEventListener("click", (event) => {
 
   navigator.clipboard.writeText(answer);
 });
+
+fetch("https://jsonplaceholder.typicode.com/todos/1")
+  .then((response) => response.json())
+  .then((json) => console.log(json));
